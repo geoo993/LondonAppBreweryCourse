979c9a7a6f0cc4979da4caad43ab303ffc3dab98
GEORGE QUENTIN <ggeoo93@gmail.com>
2017-12-22 17:44:28 +0000

    added colors in designers

---
+++ AppCore/AppCoreElement/AppCoreElement.h
@@ -0,0 +1,19 @@
+//
+//  AppCoreElement.h
+//  AppCoreElement
+//
+//  Created by GEORGE QUENTIN on 22/12/2017.
+//  Copyright © 2017 Geo Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppCoreElement.
+FOUNDATION_EXPORT double AppCoreElementVersionNumber;
+
+//! Project version string for AppCoreElement.
+FOUNDATION_EXPORT const unsigned char AppCoreElementVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppCoreElement/PublicHeader.h>
+
+
