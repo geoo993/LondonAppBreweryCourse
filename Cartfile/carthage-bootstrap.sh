760bcd2a7397f673726a21c32872d3cae48c9b0f
GEORGE QUENTIN <ggeoo93@gmail.com>
2017-12-30 22:35:27 +0000

    added carthage

---
+++ Cartfile/carthage-bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  carthage-bootstrap.sh
+#  StorySmarties
+#
+#  Created by Daniel Asher on 29/06/2016.
+#  Copyright © 2016 LEXI LABS. All rights reserved.
+
+carthage bootstrap --use-submodules --no-build --no-use-binaries --platform iOS
--- Clima/Clima/Controllers/WeatherViewController.swift
+++ Clima/Clima/Controllers/WeatherViewController.swift
@@ -7,18 +7,20 @@
 //
 
 import UIKit
+import CoreLocation
+import Alamofire
+import SwiftyJSON
 
-
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
-    let myAPP_ID = "2f935ffd0c4dccdb0c303b592d3067ff"
+    //let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    let APP_ID = "2f935ffd0c4dccdb0c303b592d3067ff"
     
 
     //TODO: Declare instance variables here
-    
+    let locationManager = CLLocationManager()
 
     
     //Pre-linked IBOutlets
@@ -30,11 +32,11 @@ class WeatherViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         //TODO:Set up the location manager here.
-    
-        
-        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
     
     
@@ -43,7 +45,32 @@ class WeatherViewController: UIViewController {
     /***************************************************************/
     
     //Write the getWeatherData method here:
-    
+    func getWeatherData(url: String, parameters: [String : String]) {
+        
+        Alamofire.request(url, method: .get, parameters: parameters).responseJSON { [weak self] response in
+            guard let this = self else { return }
+            print("Request: \(String(describing: response.request))")   // original url request
+            print("Response: \(String(describing: response.response))") // http url response
+            print("Result: \(response.result)")                         // response serialization result
+            
+            if  response.result.isSuccess {
+                print("Success, Got the weather data") // serialized json response
+            }else {
+                print("Error", response.result.error!)
+                this.cityLabel.text = "Request Failed"
+            }
+            
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+            
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)") // original server data as UTF8 string
+            }
+        }
+        
+        cityLabel.text = "Location Unavailable"
+    }
 
     
     
@@ -76,12 +103,26 @@ class WeatherViewController: UIViewController {
     
     
     //Write the didUpdateLocations method here:
-    
-    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+       
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+            let locAltitude = String(location.coordinate.latitude)
+            let locLongitute = String(location.coordinate.longitude)
+            let params : [String : String] = ["lat":locAltitude.description, 
+                                              "lon": locLongitute.description, 
+                                              "appid": APP_ID]
+            getWeatherData(url: WEATHER_URL, parameters: params)
+            
+        }
+    }
     
     //Write the didFailWithError method here:
-    
-    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location Unavailable"
+    }
     
 
     
