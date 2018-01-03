c8bb6323f9471d83125f8b9fce2e6e7fd50d5988
GEORGE QUENTIN <ggeoo93@gmail.com>
2018-01-03 18:57:23 +0000

    added firebase files to project

---
+++ Flash Chat/Flash Chat/Supporting Files/InstallingFirebase.swift
@@ -0,0 +1,12 @@
+//
+//  InstallingFirebase.swift
+//  Flash Chat
+//
+//  Created by GEORGE QUENTIN on 03/01/2018.
+//  Copyright © 2018 Geo Games. All rights reserved.
+//
+
+import Foundation
+
+// installing Firsbase without cocoapods
+// http://www.mokacoding.com/blog/setting-up-firebase-without-cocoapods/
