e769264d9f31eb0ec23cb9f9dd795cb7ee9095bf
GEORGE QUENTIN <ggeoo93@gmail.com>
2018-01-03 02:29:29 +0000

    added DeviceType extension

--- AppCore/AppCore/Extensions/CGFloat+Ext.swift
+++ AppCore/AppCore/Extensions/CGFloat+Ext.swift
@@ -17,50 +17,65 @@ public extension CGFloat {
         return Double(self)
     }
     
-    public static func recommendedHeight(withMaxHeight size: CGFloat) -> CGFloat {
-        let bounds = UIScreen.main.bounds
-        let height = bounds.size.height
-        switch height {
-        case 480.0: //Iphone 3,4 => 3.5 inch
-            return size * 0.55
-        case 568.0: //iphone 5, 5s, SE => 4 inch
-            return size * 0.64
-        case 667.0: //iphone 8, 7, 6, 6s => 4.7 inch
-            return size * 0.72
-        case 736.0: //iphone 8s, 7s, 6s+ 6+ => 5.5 inch
-            return size * 0.80
-        case 812.0: //iphone 10 inch
-            return size * 0.84
-        case 1024.0: //iPad Retina, iPad Air, iPad Mini
-            return size * 0.90
-        case 1112.0: //iPad Pro 10.5
-            return size * 0.94
-        case 1366.0: //iPad Pro 12.9
-            return size
-        default:
-            return 0
+    public static func width( ofDevice device: DeviceType) -> (width:CGFloat, exponent: CGFloat) {
+        switch device {
+        case .iPhone5, .iPhone5s, .iPhone5c, .iPhoneSE:
+            return (320.0 , 0.3125)
+        case .iPhone6, .iPhone6s, .iPhone7, .iPhone8, .iPhoneX:
+            return (375.0 , 0.36621094)
+        case .iPhone6Plus, .iPhone6sPlus, .iPhone7Plus, .iPhone8Plus:
+            return (414.0 , 0.40429688)
+        case .iPadMini, .iPadMini2, .iPadMini3, .iPadMini4, 
+             .iPad2, .iPad3, .iPad4, .iPad5, .iPadAir, .iPadAir2, .iPadPro9_7:
+            return (768.0 , 0.75)
+        case .iPadPro10_5:
+            return (834.0 , 0.81445312)
+        case .iPadPro12_9:
+            return (1024.0 , 1.0)
+        case .other, .appleTV, .appleTV4K, .simulator:
+            return (0.0, 0.0)
         }
     }
-
-    public static func recommendedWidth(withMaxWidth size: CGFloat) -> CGFloat {
-        let bounds = UIScreen.main.bounds
-        let width = bounds.size.width
-        switch width {
-        case 320.0: //Iphone 3,4 5, 5s, SE => 4 inch
-            return size * 0.60
-        case 375.0: //iphone 10, 8, 7, 6, 6s => 4.7 inch
-            return size * 0.66
-        case 414.0: //iphone 8s, 7s, 6s+ 6+ => 5.5 inch
-            return size * 0.72
-        case 768.0: //ipad Retina, ipad Air, iPad Mini
-            return size * 0.84
-        case 834.0: //iPad Pro 10.5
-            return size * 92
-        case 1024.0: //iPad Pro 12.9
-            return size
-        default:
-            return 0
+    
+    public static func height( ofDevice device: DeviceType) -> (height:CGFloat, exponent: CGFloat) {
+        switch device {
+        case .iPhone5, .iPhone5s, .iPhone5c, .iPhoneSE:
+            return (568.0 , 0.41581259)
+        case .iPhone6, .iPhone6s, .iPhone7, .iPhone8:
+            return (667.0 , 0.4992515)
+        case .iPhone6Plus, .iPhone6sPlus, .iPhone7Plus, .iPhone8Plus:
+            return (736.0 , 0.53879941)
+        case .iPhoneX:
+            return (812.0 , 0.59443631)
+        case .iPadMini, .iPadMini2, .iPadMini3, .iPadMini4, 
+             .iPad2, .iPad3, .iPad4, .iPad5, .iPadAir, .iPadAir2, .iPadPro9_7:
+            return (1024.0 , 0.74963397)
+        case .iPadPro10_5:
+            return (1112.0 , 0.81405564)
+        case .iPadPro12_9:
+            return (1366.0 , 1.0)
+        case .other, .appleTV, .appleTV4K, .simulator:
+            return (0.0, 0.0)
         }
+    }
+    
+    public static func recommenedWidth( withReferencedDevice device: DeviceType, 
+                                        desiredWidth : CGFloat? = nil ) -> CGFloat {
+        let referencedDevice = CGFloat.width(ofDevice: device)
+        let referencedDeviceWidth = desiredWidth ?? CGFloat.width(ofDevice: device).width
+        let size = referencedDeviceWidth / referencedDevice.exponent
+        let currentDevice = UIDevice.current.modelName
+        let currentDeviceWidthExponent = CGFloat.width(ofDevice: currentDevice).exponent
+        return size * currentDeviceWidthExponent
+    }
+    public static func recommenedHeight(withReferencedDevice device: DeviceType, 
+                                        desiredHeight: CGFloat? = nil) -> CGFloat {
+        let referencedDevice = CGFloat.height(ofDevice: device)
+        let referencedDeviceHeight = desiredHeight ?? CGFloat.height(ofDevice: device).height
+        let size = referencedDeviceHeight / referencedDevice.exponent
+        let currentDevice = UIDevice.current.modelName
+        let currentDeviceHeightExponent = CGFloat.height(ofDevice: currentDevice).exponent
+        return size * currentDeviceHeightExponent
     }
     
     public func round(to places:Int) -> CGFloat {
@@ -95,9 +110,9 @@ public extension CGFloat {
         return (CGFloat(100) * ((self - minValue) / difference));
     }
     
-    public func percentangeFromMaxValue(withPercentage percent : CGFloat, minValue : CGFloat) -> CGFloat {
-        let max = (self > minValue) ? self : minValue
-        let min = (self > minValue) ? minValue : self
+    public static func percentangeFromMaxValue(withPercentage percent : CGFloat, maxValue : CGFloat, minValue : CGFloat) -> CGFloat {
+        let max = (maxValue > minValue) ? maxValue : minValue
+        let min = (maxValue > minValue) ? minValue : maxValue
         
         return ( ((max - min) * percent) / CGFloat(100) ) + min
     }
